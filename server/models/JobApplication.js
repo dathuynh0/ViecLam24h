2373be18b16b7252419973a0ca32848b21845521
dathuyn0 <hcdattv@gmail.com>
2026-06-29 13:50:05 +0700

    Cập nhật các models

---
+++ server/models/JobApplication.js
@@ -0,0 +1,58 @@
+import sequelize from "../config/db.js";
+import { DataTypes } from "sequelize";
+import crypto from "crypto"
+
+import Candidate from './Candidate.js'
+import Job from "./Job.js";
+
+const generateId = () => crypto.randomBytes(5).toString('hex')
+
+const JobApplication = sequelize.define('JobApplication', {
+    id: {
+        type: DataTypes.STRING(16),
+        primaryKey: true,
+        defaultValue: () => generateId
+    },
+    candidateId: {
+        type: DataTypes.STRING(16),
+        allowNull: false,
+        references: {
+            model: Candidate,
+            key: 'id'
+        }
+    },
+    jobId: {
+        type: DataTypes.STRING(16),
+        allowNull: false,
+        references: {
+            model: Job,
+            key: 'id'
+        }
+    },
+    applyCVUrl: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.ENUM('pending', 'reviewing', 'interviewing', 'accepted', 'rejected'),
+        defaultValue: 'pending'
+    }
+},
+{
+    indexes: [
+        {
+            unique: true,
+            fields: ['candidateId', 'jobId']
+        }
+    ],
+    timestamps: true
+});
+
+// candidate 1 - n JobApplication
+Candidate.hasMany(JobApplication, { foreignKey: 'candidateId', as: 'application' })
+JobApplication.belongsTo(Candidate, { foreignKey: 'candidateId', as: 'candidate' })
+
+// jobApplication 1 - 1 job
+JobApplication.belongsTo(Job, { foreignKey: 'jobId', as: 'job' })
+
+export default JobApplication;
---
+++ server/models/JobSave.js
@@ -0,0 +1,54 @@
+import sequelize from "../config/db.js";
+import { DataTypes } from "sequelize";
+import crypto from "crypto"
+
+import Candidate from './Candidate.js'
+import Job from "./Job.js";
+
+const generateId = () => crypto.randomBytes(5).toString('hex')
+
+const JobSave = sequelize.define('JobSave', {
+    id: {
+        type: DataTypes.STRING(16),
+        primaryKey: true,
+        defaultValue: () => generateId()
+    },
+    candidateId: {
+      type: DataTypes.STRING(16),
+      allowNull: false,
+      references: {
+        model: Candidate,
+        key: 'id'
+      }
+    },
+    jobId: {
+      type: DataTypes.STRING(16),
+      allowNull: false,
+      references: {
+        model: Job,
+        key: 'id'
+      }
+    },
+    savedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+},
+{
+    indexes: [
+        {
+            unique: true,
+            fields: ['candidateId', 'jobId']
+        }
+    ]
+});
+
+// candidate n - 1 jobSave
+Candidate.hasMany(JobSave, { foreignKey: 'candidateId', as: 'jobSaves'})
+JobSave.belongsTo(Candidate, { foreignKey: 'candidateId', as: 'candidate'})
+
+// job 1 - n jobsave
+Job.hasMany(JobSave, { foreignKey: 'jobId', as: 'jobSaves' })
+JobSave.belongsTo(Job, { foreignKey: 'jobId', as: 'job' })
+
+export default JobSave;
