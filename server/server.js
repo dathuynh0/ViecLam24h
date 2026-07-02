24ec4e62e445801ed7d88d48be37923bd96a490f
QueAnh068 <thachnguyenqueanh2005@gmail.com>
2026-07-02 20:10:13 +0700

    Thêm chức năng đăng nhập

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
 import "dotenv/config";
 import express from "express";
 import sequelize from "./config/db.js";
-import authRoutes from "./routes/authRoute.js";
+import jobRoutes from "./routes/jobRoutes.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
+// API quản lý tuyển dụng: Đăng bài, Sửa/Xóa bài, Xem danh sách bài đăng
+app.use("/api/jobs", jobRoutes);
 
-app.use('/api/auth', authRoutes);
+app.get("/", (req, res) => {
+  res.send("API ViecLam24h đang chạy");
+});
 
-
-sequelize.sync({ alter: true })
+sequelize.sync({ alter: false })
   .then(() => {
     app.listen(PORT, () => {
-    console.log(`Server đang chạy trên port http://localhost:${PORT}`);
+      console.log(`Server đang chạy trên port http://localhost:${PORT}`);
     });
   })
   .catch((error) => console.error("Lỗi khi kết nối với CSDL: ", error));
