031e95a26cba962a50ddcc41c722e87624017ff6
QueAnh068 <thachnguyenqueanh2005@gmail.com>
2026-07-03 15:04:00 +0700

    Thêm chức năng đăng nhập

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import sequelize from "./config/db.js";
 import jobRoutes from "./routes/jobRoutes.js";
+import applicationRoutes from "./routes/applicationRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,6 +11,9 @@ app.use(express.json());
 
 // API quản lý tuyển dụng: Đăng bài, Sửa/Xóa bài, Xem danh sách bài đăng
 app.use("/api/jobs", jobRoutes);
+
+// API ứng tuyển: nộp CV, danh sách CV đã nộp, trạng thái CV
+app.use("/api/applications", applicationRoutes);
 
 app.get("/", (req, res) => {
   res.send("API ViecLam24h đang chạy");
